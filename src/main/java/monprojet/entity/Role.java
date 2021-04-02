ff27b1c719cd90c49e4a649c91306ae74bc6def8
Rémi Bastide <Remi.Bastide@irit.fr>
2021-04-02 19:47:35 +0200

    Initialisation des rôles dans UserServiceImpl plutôt que dans data.sql

--- src/main/java/monprojet/entity/Role.java
+++ src/main/java/monprojet/entity/Role.java
@@ -23,7 +23,7 @@ import lombok.ToString;
 @Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Setter(AccessLevel.NONE)
     private Long id;
 
